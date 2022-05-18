055ddef7dfc887a0765b0bb179f444695c25b0d4
yazawa <nicoberndt234@gmail.com>
2022-05-18 17:29:07 +0200

    final changes

--- app/_6_YourOwnAnimation.java
+++ app/_6_YourOwnAnimation.java
@@ -110,6 +110,26 @@ public class _6_YourOwnAnimation extends Animation {
 }
 
 @SuppressWarnings("serial")
+class SecondaryGraphicsContent extends JPanel {
+	// set this panel's preferred size for auto-sizing the container JFrame
+	public Dimension getPreferredSize() {
+		return new Dimension(300, 100);
+	}
+
+	// drawing operations should be done in this method
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		double masseschwerpunkt_M1 = _6_YourOwnAnimationPanel.x1t + 25;
+		double masseschwerpunkt_M2 = _6_YourOwnAnimationPanel.x2t + 25;
+		g.drawString("Massenmittelpunkt von Masse 1: x: " + (int)masseschwerpunkt_M1 + "  y: " + 225, 10, 20);
+		g.drawString("Massenmittelpunkt von Masse 2: x: " + (int)masseschwerpunkt_M2 + "  y: " + 225, 10, 35);
+		g.drawString("Schwerpunktposition: " + _6_YourOwnAnimationPanel.sppX, 10, 50);
+		g.setColor(Color.LIGHT_GRAY);
+	}
+}
+
+@SuppressWarnings("serial")
 class _6_YourOwnAnimationPanel extends JPanel {
 
 	// panel has a single time tracking thread associated with it
@@ -156,10 +176,16 @@ class _6_YourOwnAnimationPanel extends JPanel {
 	
 	//Effektiver Abstand s(t)
 	public static double d = 20;
+	double phaseangle = 1;
 	double a = 1;
 	double b = 1;
 	double u = 1;
 	double s = 1;
+
+	// Diagramm
+	double[][] koordinaten1 = new double[200][200];
+	double[][] koordinaten2 = new double[200][200];
+	double[][] koordinaten3 = new double[200][200];
 
 	// drawing operations should be done in this method
 	@Override
@@ -174,6 +200,29 @@ class _6_YourOwnAnimationPanel extends JPanel {
 		
 		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect(0, 0, width, height);
+
+		/*// für das Diagramm
+		for (int i=0; i<=400; i++){
+			//Schwerpunktposition X(t)
+			masseinvers = (1/(mass1 + mass2));
+			sppX = masseinvers * (mass1*x1_start + mass2*x2_start) + masseinvers * (mass1*v1 + mass2*v2) * i/32;
+		
+			//Effektive Abstand s(t)
+			u = (mass1 * mass2) / (mass1 + mass2);
+			a = x2_start - x1_start - l0;
+			b = Math.sqrt(u/d) * (v2 - v1);
+			double phaseangle2 = Math.sqrt(d/u) * i/32;
+			s = a * Math.cos(phaseangle2) + b * Math.sin(phaseangle2);
+		
+			// Position der beiden Massen
+			x1t = sppX - (mass2/(mass1 + mass2)) * (s + l0);
+			x2t = sppX + (mass1/(mass1 + mass2)) * (s + l0);
+
+			g.setColor(Color.yellow);
+			g.fillOval(width - 300 + i, height - 50 - (int)(x1t/2.5) + 80, 4, 4);
+			g.setColor(Color.green);
+			g.fillOval(width - 300 + i, height - 50 - (int)(x2t/2.5) + 80, 4, 4);
+		}*/
 		
 		//Schwerpunktposition X(t)
 		masseinvers = (1/(mass1 + mass2));
@@ -183,7 +232,7 @@ class _6_YourOwnAnimationPanel extends JPanel {
 		u = (mass1 * mass2) / (mass1 + mass2);
 		a = x2_start - x1_start - l0;
 		b = Math.sqrt(u/d) * (v2 - v1);
-		double phaseangle = Math.sqrt(d/u) * time;
+		phaseangle = Math.sqrt(d/u) * time;
 		s = a * Math.cos(phaseangle) + b * Math.sin(phaseangle);
 		
 		// Position der beiden Massen
@@ -196,7 +245,7 @@ class _6_YourOwnAnimationPanel extends JPanel {
 
 		// Validierung
 		sspV = (1/(mass1 + mass2)) * (mass1 * v1 + mass2 * v2);
-		schwingAmplitude = s * Math.sin(Math.sqrt(d/u) * time);
+		schwingAmplitude = Math.sqrt(a*a + b*b);
 		schwingT = 2 * Math.PI * Math.sqrt(mass1/d);
 		schwingT2 = 2 * Math.PI * Math.sqrt(mass2/d);
 
@@ -225,10 +274,10 @@ class _6_YourOwnAnimationPanel extends JPanel {
 			g.drawString("da eine Kollision,", 100, 200);
 			g.drawString("der beiden Massen stattfindet!!!", 100, 300);
 		}
-
+		
 		g.setColor(Color.blue);
 		g.setFont(new Font("Arial", Font.PLAIN, 20));
-		g.drawString("Zeitpunkt: " + time, 10, height - 125);
+		g.drawString("Zeitpunkt: " + (int)time, 10, height - 125);
 		g.drawString("Schwerpunktgeschwindigkeit: " + sspV, 10, height - 100);
 		g.drawString("Schwingungsamplitude: " + schwingAmplitude, 10, height - 75);
 		g.drawString("Schwingperiode Masse 1: " + schwingT, 10, height - 50);
@@ -239,46 +288,50 @@ class _6_YourOwnAnimationPanel extends JPanel {
 		g.setFont(new Font("Arial", Font.PLAIN, 10));
 		g2d.setStroke(new BasicStroke(5.0f));
 		g.drawLine(width - 400, height - 50, width, height - 50);
-		g.drawLine(width - 200, height - 250, width - 200, height);
+		g.drawLine(width - 300, height - 250, width - 300, height);
 
-		g.drawString("200", width - 225, height - 50 - 20);
-		g.drawString("400", width - 225, height - 50 - 40);
-		g.drawString("600", width - 225, height - 50 - 60);
-		g.drawString("800", width - 225, height - 50 - 80);
-		g.drawString("x", width - 211, height - 50 - 150);
+		// Zeichne y-Achsenbeschriftung
+		int offset2 = 40;
+		for (int i=3; i<=6; i++){
+			String s = Integer.toString(i * 100);
+			g.drawString(s, width -325, height -50 - offset2);
+			offset2 += 40;
+		}
+		g.drawString("x", width - 311, height - 50 - 200);
 
-		g.drawString("5", width - 200 + 10, height - 50 + 11);
-		g.drawString("10", width - 200 + 20, height - 50 + 11);
-		g.drawString("20", width - 200 + 40, height - 50 + 11);
-		g.drawString("30", width - 200 + 60, height - 50 + 11);
-		g.drawString("40", width - 200 + 80, height - 50 + 11);
-		g.drawString("50", width - 200 + 100, height - 50 + 11);
-		g.drawString("60", width - 200 + 120, height - 50 + 11);
-		g.drawString("t", width - 200 + 180, height - 50 + 11);
+		// Zeichne x-Achsenbeschriftung
+		int offset = 24;
+		for (int i=1; i<=10; i++){
+			String s = Integer.toString(i);
+			g.drawString(s, width - 300 + offset, height - 50 + 11);
+			offset += 24;
+		}
+		g.drawString("t", width - 300 + 280, height - 50 + 11);
 
-		g.setColor(Color.red);
-		g.fillOval(width - 200 + (int)time*2, height - 50 - (int)x1t/10, 10, 10);
-		g.setColor(Color.blue);
-		g.fillOval(width - 200 + (int)time*2, height - 50 - (int)x2t/10, 10, 10);
+		// Zeichne Kurve
+		for (int i=0; i<=400; i++){
+			//Schwerpunktposition X(t)
+			// i/32 macht, dass ich 400* den x-Wert aufgeteilt auf die ersten 12.5 sekunden bekomme.
+			sppX = masseinvers * (mass1*x1_start + mass2*x2_start) + masseinvers * (mass1*v1 + mass2*v2) * i/32;
+		
+			//Effektive Abstand s(t)
+			phaseangle = Math.sqrt(d/u) * i/32;
+			s = a * Math.cos(phaseangle) + b * Math.sin(phaseangle);
+		
+			// Position der beiden Massen
+			x1t = sppX - (mass2/(mass1 + mass2)) * (s + l0);
+			x2t = sppX + (mass1/(mass1 + mass2)) * (s + l0);
+
+			// masse 1
+			g.setColor(Color.red);
+			g.fillOval(width - 300 + i, height - 50 - (int)(x1t/2.5) + 80, 4, 4);
+			// masse 2
+			g.setColor(Color.blue);
+			g.fillOval(width - 300 + i, height - 50 - (int)(x2t/2.5) + 80, 4, 4);
+			// schwerpunktposition
+			g.setColor(Color.BLACK);
+			g.fillOval(width - 300 + i, height - 50 - (int)(sppX/2.5) + 80, 4, 4);
+		}
 	}
 }
 
-@SuppressWarnings("serial")
-class SecondaryGraphicsContent extends JPanel {
-	// set this panel's preferred size for auto-sizing the container JFrame
-	public Dimension getPreferredSize() {
-		return new Dimension(300, 100);
-	}
-
-	// drawing operations should be done in this method
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		double masseschwerpunkt_M1 = _6_YourOwnAnimationPanel.x1t + 25;
-		double masseschwerpunkt_M2 = _6_YourOwnAnimationPanel.x2t + 25;
-		g.drawString("Massenmittelpunkt von Masse 1: x: " + (int)masseschwerpunkt_M1 + "  y: " + 225, 10, 20);
-		g.drawString("Massenmittelpunkt von Masse 2: x: " + (int)masseschwerpunkt_M2 + "  y: " + 225, 10, 35);
-		g.drawString("Schwerpunktposition: " + _6_YourOwnAnimationPanel.sppX, 10, 50);
-		g.setColor(Color.LIGHT_GRAY);
-	}
-}
